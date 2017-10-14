1929466c0ee8fee4a278e8f3c3768156a07a97ef
Josh Lee <josh@joshuamlee.com>
2017-10-14 03:46:37 -0400

    adds prod config

--- config/prod.exs
+++ config/prod.exs
@@ -23,7 +23,7 @@ config :logger, level: :info
 
 config :soiled_api, SoiledApi.Repo,
   adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("DATABASE_URL")
+  url: System.get_env("DATABASE_URL"),
   pool_size: 10,
   extensions: [{Geo.PostGIS.Extension, library: Geo}],
   types: SoiledApi.PostgresTypes
